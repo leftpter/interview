ae5ff06b2e59e49dfe8d6e9d3ba9b1912ad91287
Peter Left <left.peter@gmail.com>
2014-09-01 23:10:49 -0500

    first version of code

---
+++ Demo/src/com/peter/left/demo/Demo.java
@@ -0,0 +1,11 @@
+package com.peter.left.demo;
+import java.util.function.*;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		final Function func = (n) -> System.out.print(n);
+
+	}
+
+}
